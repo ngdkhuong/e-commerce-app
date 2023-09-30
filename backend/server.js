42f5274a5d2808f3d9746e5d4ae98c078574cce7
Dang Khuong Nguyen <nguyenkhuong31072002@gmail.com>
2023-09-30 10:44:03 +0700

    fix cors policy

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dbConnect = require('./config/dbConnect');
 const { notFound, errorHandler } = require('./middleware/errorHandler');
 const userRouter = require('./router/userRoutes');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 4000;
@@ -11,8 +12,17 @@ dbConnect();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// Middleware for handling CORS POLICY
+app.use(
+    cors({
+        origin: 'http://localhost:5173',
+        credentials: true,
+    }),
+);
+
 // GET testing server
-app.get('/', (req, res) => {
+app.get('/api/test', (req, res) => {
     res.send('Welcome to LMS Server! Suka Blyad');
 });
 

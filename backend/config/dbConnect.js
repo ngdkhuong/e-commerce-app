46e5a541330cc29d13121cdd2a5e6120480bbc99
Dang Khuong Nguyen <nguyenkhuong31072002@gmail.com>
2023-10-21 16:41:09 +0700

    push

--- backend/config/dbConnect.js
+++ backend/config/dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const dbConnect = () => {
     try {
-        const connection = mongoose.connect(process.env.MONGO_URL);
+        const connection = mongoose.connect('mongodb://mongo-db/LMS');
         console.log('Connected to MongoDB');
     } catch (error) {
         console.log(error);
